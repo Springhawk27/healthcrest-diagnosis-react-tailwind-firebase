3cab09ae0f7870e5cd0113c114c2b283bb7ac1ae
Springhawk27 <sajjad0mahmud@gmail.com>
2021-10-18 15:33:16 +0600

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h2>Welcome to the Healthcrest Diagnosis</h2>
     </div>
   );
