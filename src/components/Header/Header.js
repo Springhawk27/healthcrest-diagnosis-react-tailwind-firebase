7e4b23bc9a1ed2a0abe9a65044da896f6a8da8c5
Springhawk27 <sajjad0mahmud@gmail.com>
2021-10-18 17:04:33 +0600

    basic component creation done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from 'react';
+import main_logo from '../../images/logo/main-logo.png'
 
 const Header = () => {
     return (
-        <div>
-            <header class="text-gray-600 body-font bg-red-50	">
-                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="sticky top-0	"	>
+            <header class="text-gray-600 body-font bg-green-50 ">
+                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
                     <div class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span class="ml-3 text-xl">Tailblocks</span>
+                        <img clas src={main_logo} alt="" />
+                        <span class="ml-3 text-xl text-green-600">Healthcrest Diagnosis</span>
                     </div>
                     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                         <div class="mr-5 hover:text-gray-900">Fourth Link</div>
@@ -18,7 +16,7 @@ const Header = () => {
                         <div class="mr-5 hover:text-gray-900">Fourth Link</div>
                         <div class="mr-5 hover:text-gray-900">Fourth Link</div>
                     </nav>
-                    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+                    <button class="inline-flex items-center bg-green-100 border-0 py-1 px-3 focus:outline-none hover:bg-green-200 rounded text-base mt-4 md:mt-0">Button
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
