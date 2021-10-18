18d483fd66ee3e9b1f9d5091218a2c0177f9e7f1
Springhawk27 <sajjad0mahmud@gmail.com>
2021-10-18 19:15:50 +0600

    routing and not found page done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Doctors from './components/Doctors/Doctors';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
@@ -37,6 +38,7 @@ function App() {
             <NotFound></NotFound>
           </Route>
         </Switch>
+        <Footer></Footer>
       </Router>
     </div>
   );
